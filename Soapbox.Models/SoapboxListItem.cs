94be4829da3b70186dee3dd0ee75a928a1ca360f
stephenmcfarland <snmcfarland@gmail.com>
2019-06-10 14:40:49 -0400

    MVC no auth

---
+++ Soapbox.Models/SoapboxListItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Soapbox.Models
+{
+    public class SoapboxListItem
+    {
+        public int SoapboxId { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
