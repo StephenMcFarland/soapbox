7ebccbaf9cc9216cf14150bda9bd5d8e22f41506
stephenmcfarland <snmcfarland@gmail.com>
2019-06-10 14:24:59 -0400

    soapbox

--- Soapbox.Data/soapbox.cs
+++ Soapbox.Data/soapbox.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Soapbox.Data
 {
     public class Soapbox
     {
+        [Key]
         public int SoapboxId { get; set; }
-        public Guid OwnerId { get; set; }
+
+        //[Required]
+        //public Guid OwnerId { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Content { get; set; }
 
         public string ImageUrl { get; set; }
 
+        [Required]
         public DateTimeOffset CreatedUtc { get; set; }
 
         public DateTimeOffset? ModifiedUtc { get; set; }
---
+++ Soapbox/Controllers/SoapboxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Soapbox.Controllers
+{
+    public class SoapboxController : Controller
+    {
+        // GET: Soapbox
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
