19d1f963145c5262e89829fcb66937cf9fd88c0e
stephenmcfarland <snmcfarland@gmail.com>
2019-06-10 12:44:31 -0400

    MVC no auth

---
+++ Soapbox.Data/soapbox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soapbox.Data
+{
+    public class Soapbox
+    {
+        public int SoapboxId { get; set; }
+        public Guid OwnerId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
